6922b19e5046c715b54f4f60353530c54f9b7823
github username <github email address>
2020-05-19 11:31:29 -0700

    Done.

---
+++ app/models/school_class.rb
@@ -0,0 +1,7 @@
+class SchoolClass < ApplicationRecord
+
+    # def initialize(args)
+    #     @title = args[:school_class][:title]
+    #     @room_number = [:school_class][:room_number]
+    # end
+end
